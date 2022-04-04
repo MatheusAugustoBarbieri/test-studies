e2c007865e66cfe352a09b0dd9a4e2ecc774fa25
MatheusAugustoBarbieri <barbiericonta@hotmail.com>
2022-04-03 23:03:36 -0300

    first commit moduleTwo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
